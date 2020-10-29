3f0a0b4bf44e11b3556e89705e8a4aad91997430
Susan Wiens <susanmichellewiens@gmail.com>
2020-10-29 15:04:13 -0500

    refactored code in park.js, parkSelect.js and parkList.js. fixed event listener

--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -1,8 +1,8 @@
 export const Park = (parkObj) => {
     return `
     <div class="park">
-            <p class="park__name"><h3>${parkObj.fullName}</h3></p>
-            <p class="park__image">${parkObj.images[0]}</p>
+            <p class="park__name">${parkObj.fullName}</p>
+            <img src="${parkObj.images[0].url}">
     </div>
     `
 }
